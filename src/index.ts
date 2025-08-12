b38a4fadb92a8f78155680a2b916dfa943c401f0
anyme <656538470@qq.com>
2025-08-12 11:23:40 +0800

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import "./env";
 import { DI } from "./inversify.config";
-import express from "express";
 export { defineConfig, ENC } from "./utils";
-export const ready = DI.createApp;
-ready(express()).then((app) => {
-  app.bootstrap();
-});
+export const createApp = DI.createApp;
