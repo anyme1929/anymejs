220d629af8465775cbb8f490b3556c0076a0f9e1
anyme <656538470@qq.com>
2025-08-13 14:38:44 +0800

    refactor(src): 移除 index.ts 中的 express 导入和应用启动代码
    - 删除了 express 的导入语句
    - 移除了 createApp 和应用启动的相关代码

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ import "./env";
 import { DI } from "./inversify.config";
 export { defineConfig, ENC } from "./utils";
 export const createApp = DI.createApp;
-import express from "express";
-createApp(express()).then((app) => {
-  app.bootstrap();
-});
